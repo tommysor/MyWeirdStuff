4372a5e381bec467be1246f385c19120f5889f24
Tommy Sørbråten <tommysor@gmail.com>
2024-04-28 13:11:06 +0000

    force scheme http for call to apiservice

--- src/MyWeirdStuff.Web/Program.cs
+++ src/MyWeirdStuff.Web/Program.cs
@@ -16,7 +16,8 @@ builder.Services.AddHttpClient<WeatherApiClient>(client =>
     {
         // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
         // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
+        // client.BaseAddress = new("https+http://apiservice");
+        client.BaseAddress = new("http://apiservice");
     });
 
 var app = builder.Build();
