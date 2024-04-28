96bffae999cae34c7f06495842f1ec1b89bd18c6
Tommy Sørbråten <tommysor@gmail.com>
2024-04-28 12:51:00 +0000

    Add missing using statement

--- src/MyWeirdStuff.ServiceDefaults/Extensions.cs
+++ src/MyWeirdStuff.ServiceDefaults/Extensions.cs
@@ -1,3 +1,4 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
