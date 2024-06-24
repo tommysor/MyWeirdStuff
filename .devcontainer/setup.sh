0472ae38a1a9a722bd37e042bb6f50376f8dbce9
Tommy Sørbråten <tommysor@gmail.com>
2024-06-24 19:54:51 +0000

    force aspire version when building codespace

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,1 +1,3 @@
-dotnet workload install aspire
+echo '{"microsoft.net.sdk.aspire": "8.0.1/8.0.100"}' > ./aspireVersion55168915364.txt
+dotnet workload install aspire --from-rollback-file ./aspireVersion55168915364.txt
+rm ./aspireVersion55168915364.txt
