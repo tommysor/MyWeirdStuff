33407f38aac63889bf89aa2514870ccba4d08b5d
Tommy Sørbråten <tommysor@gmail.com>
2024-07-03 20:58:49 +0000

    Add map AddComicEvent tests

--- src/MyWeirdStuff.ApiService.Tests/Infrastructure/TableEntityToEventMapperTests.cs
+++ src/MyWeirdStuff.ApiService.Tests/Infrastructure/TableEntityToEventMapperTests.cs
@@ -7,12 +7,13 @@ namespace MyWeirdStuff.ApiService.Tests.Infrastructure;
 public class TableEntityToEventMapperTests
 {
     private readonly TableEntity _tableEntity;
+    private const string _tableEntityPartitionKey = "xkcd.com-1";
 
     public TableEntityToEventMapperTests()
     {
         _tableEntity = new TableEntity
         {
-            PartitionKey = "xkcd.com-1",
+            PartitionKey = _tableEntityPartitionKey,
             RowKey = "20240703123456789",
             Timestamp = DateTimeOffset.UtcNow,
             ETag = new Azure.ETag("etag"),
@@ -30,5 +31,62 @@ public class TableEntityToEventMapperTests
         
         // Then
         Assert.IsType<ComicAddedEvent>(actual);
+    }
+
+    [Fact]
+    public void Map_WhenComicAddedEvent_ThenIdAndUrlShouldBeMapped()
+    {
+        // When
+        var returned = TableEntityToEventMapper.Map(_tableEntity);
+
+        // Then
+        var actual = Assert.IsType<ComicAddedEvent>(returned);
+        Assert.Equal(_tableEntityPartitionKey, actual.PartitionKey);
+        Assert.Equal("https://xkcd.com/1/", actual.Url);
+    }
+
+    [Fact]
+    public void Map_WhenEventTypeMissing_ShouldDefaultToComicAddedEvent()
+    {
+        // Given
+        _tableEntity.Remove("EventType");
+        _tableEntity.Remove("EventTypeVersion");
+        
+        // When
+        var actual = TableEntityToEventMapper.Map(_tableEntity);
+        
+        // Then
+        var comicAddedEvent = Assert.IsType<ComicAddedEvent>(actual);
+        Assert.Equal(_tableEntityPartitionKey, comicAddedEvent.PartitionKey);
+    }
+
+    [Fact]
+    public void Map_WhenEventTypeVersionMissing_ShouldDefaultToComicAddedEvent()
+    {
+        // Given
+        _tableEntity.Remove("EventTypeVersion");
+        
+        // When
+        var actual = TableEntityToEventMapper.Map(_tableEntity);
+        
+        // Then
+        var comicAddedEvent = Assert.IsType<ComicAddedEvent>(actual);
+        Assert.Equal(_tableEntityPartitionKey, comicAddedEvent.PartitionKey);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(0)]
+    public void Map_WhenEventTypeVersionNullOrZero_ShouldDefaultToComicAddedEvent(int? eventTypeVersion)
+    {
+        // Given
+        _tableEntity["EventTypeVersion"] = eventTypeVersion;
+        
+        // When
+        var actual = TableEntityToEventMapper.Map(_tableEntity);
+        
+        // Then
+        var comicAddedEvent = Assert.IsType<ComicAddedEvent>(actual);
+        Assert.Equal(_tableEntityPartitionKey, comicAddedEvent.PartitionKey);
     }
 }
